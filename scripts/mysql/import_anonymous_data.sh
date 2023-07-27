6d239a9c811aefb7335ca62c006c9114e41a93a5
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-07-27 13:38:19 +0200

    chore(scripts): extract functions to call importing data from ci

---
+++ scripts/mysql/import_anonymous_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+
+source scripts/utils.sh
+
+TMP_DIR=$(mktemp -d -t serlo-anonymous-dataXXXXXX)
+TMP_DUMP="${TMP_DIR}/dump.zip"
+LATEST_DUMP=$(gsutil ls -l gs://anonymous-data \
+	| grep dump \
+	| sort -rk 2 \
+	| head -n 1 \
+	| awk '{ print $3 }')
+
+gsutil cp "${LATEST_DUMP}" "${TMP_DUMP}"
+unzip -d "${TMP_DIR}" "${TMP_DUMP}"
+
+exec_mysql serlo < "${TMP_DIR}/mysql.sql"
+docker cp "${TMP_DIR}/user.csv" $(docker-compose ps -q mysql):/
+exec_mysql -e "LOAD DATA LOCAL INFILE 'user.csv' INTO TABLE user \
+				FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\n' \
+				IGNORE 1 ROWS;" serlo
+echo "Let's wait for the postgres database to be ready"
+sleep 30
+docker compose exec -T postgres psql --user serlo kratos < "${TMP_DIR}/kratos.sql"
+rm -r ${TMP_DIR}
