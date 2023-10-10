f836c905d267a6f1b7fe5b77f6c09f1e0fff7e27
Hugo Tiburtino <hugotiburtino@protonmail.com>
2023-10-10 19:34:33 +0200

    chore: remove wrongly added line

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,5 +1,3 @@
 function exec_mysql {
 	docker compose exec -T mysql mysql --user=root --password=secret --local-infile=1 "$@"
 }
-
-mysqldump --protocol=tcp --port=7777 --user=serlo --password --lock-all-tables --set-gtid-purged=OFF serlo > /tmp/dump-before-migration-0.3.1.sql
